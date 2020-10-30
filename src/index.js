e79cef339728f53c93d282f3fe63af55ecb912e2
David Barratt <david@davidwbarratt.com>
2020-10-30 12:20:02 -0400

    Fix eslint errors

--- src/index.js
+++ src/index.js
@@ -6,8 +6,9 @@ import { Subject } from 'rxjs';
  *
  * @param {callback} reaction Must return an instance of Subject
  * @param {callback} dispatch Will be called with the result of the reactor.
- * @param {any} input Item or an array of items that will be emitted to the reactor when any of them change.
- *                    The item or items will be compared shallowly to determine if a new emit should take place.
+ * @param {any} input Item or an array of items that will be emitted to the reactor
+ *                    when any of them change. The item or items will be compared shallowly
+ *                    to determine if a new emit should take place.
  */
 export default function useReactor(reaction, dispatch, input) {
   const subjectRef = useRef();
